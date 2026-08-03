6c34a04aeb88786fa16e40179cad5473ffef4dd1
KavyaUlleru <kavyaulleru@gmail.com>
2026-08-03 10:39:29 +0530

    Time: 8 ms (96.34%), Space: 49.4 MB (12.76%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
@@ -17,35 +17,42 @@ class Solution {
     public int maxLevelSum(TreeNode root) {
          List<List<Integer>> res=new ArrayList<>();
         Queue<TreeNode> q=new ArrayDeque<>();
+        int lev=0,reslev=0,max=Integer.MIN_VALUE;
         if(root==null)
             return 0;
         q.offer(root);
         while(!q.isEmpty()){
             int s=q.size();
-            List<Integer> list=new ArrayList<>();
+            lev++;
+            // List<Integer> list=new ArrayList<>();
+            int sum=0;
             for(int i=0;i<s;i++){
                 TreeNode temp=q.poll();
-                list.add(temp.val);
+                // list.add(temp.val);
+                sum+=temp.val;
                  if(temp.left!=null)
                     q.add(temp.left);
                 if(temp.right!=null)
                     q.add(temp.right);
                 
             }
-            res.add(list);
-        }
-        int max=Integer.MIN_VALUE;
-        int lev=0;
-        for(int i=0;i<res.size();i++){
-            int sum=0;
-            for(int j=0;j<res.get(i).size();j++){
-                sum+=res.get(i).get(j);
-            }
             if(sum>max){
                 max=sum;
-                lev=i+1;
+                reslev=lev;
             }
         }
-        return lev;
+        // int max=Integer.MIN_VALUE;
+        // int lev=0;
+        // for(int i=0;i<res.size();i++){
+        //     int sum=0;
+        //     for(int j=0;j<res.get(i).size();j++){
+        //         sum+=res.get(i).get(j);
+        //     }
+        //     if(sum>max){
+        //         max=sum;
+        //         lev=i+1;
+        //     }
+        // }
+        return reslev;
     }
 }
